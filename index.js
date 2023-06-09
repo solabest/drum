d69a2027320fc8e9fdfaaf0368a543553bdd59c7
solabest <soltad65@gmail.com>
2023-06-09 04:45:01 -0800

    add sound

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 for(var i=0;i<document.querySelectorAll(".drum").length;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function (){
-        alert("clicked!");
+        var audio=new Audio("sounds/tom-1.mp3");
+        audio.play();
     });
 }
